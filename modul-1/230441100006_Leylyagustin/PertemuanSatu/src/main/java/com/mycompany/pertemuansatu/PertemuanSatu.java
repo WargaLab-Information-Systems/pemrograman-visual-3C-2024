0b485158eaeeb31c927c4569916af2e0b1aed48d
Acer <leylyagustin23@gmail.com>
2024-09-18 17:40:01 +0700

    latihan

---
+++ modul-1/230441100006_Leylyagustin/PertemuanSatu/src/main/java/com/mycompany/pertemuansatu/PertemuanSatu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuansatu;
+
+/**
+ *
+ * @author Leyli Agustin
+ */
+public class PertemuanSatu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
