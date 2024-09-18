fcdfc1bb382e1c8265187a829020971166ae2a63
liyarafika <liyarafika1@gmail.com>
2024-09-18 17:41:03 +0700

    latihan

---
+++ modul-1/230441100145_LiyaRafikasari/pertemuansatu/src/main/java/com/mycompany/pertemuansatu/Pertemuansatu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuansatu;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Pertemuansatu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
