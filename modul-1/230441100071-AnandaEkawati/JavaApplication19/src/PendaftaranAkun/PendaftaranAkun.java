9e1e85182282add107f417f8e2c3e2c0b3950df3
Ananda071 <ekawatiananda60@gmail.com>
2024-09-30 18:36:25 +0700

    230441100071-AnandaEkawati-ImanuelMartinJ.Harahap-MODUL1

--- modul-1/230441100071-AnandaEkawati/JavaApplication19/src/PendaftaranAkun/PendaftaranAkun.java
+++ modul-1/230441100071-AnandaEkawati/JavaApplication19/src/PendaftaranAkun/PendaftaranAkun.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package pertemuanpertamana;
+package PendaftaranAkun;
 
 /**
  *
  * @author asus
  */
-public class PertemuanPertamana {
+public class PendaftaranAkun {
 
     /**
      * @param args the command line arguments
