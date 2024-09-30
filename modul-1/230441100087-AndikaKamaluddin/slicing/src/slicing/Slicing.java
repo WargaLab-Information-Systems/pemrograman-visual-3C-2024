2829e2fa9658e7630ca188aad4b316714173bf49
yukiinon <andikakamaluddin1@gmail.com>
2024-09-30 15:10:47 +0700

    230441100087-ANDIKAKAMALUDDIN-IMANUELMARTINJ.HARAHAP-MODUL1

--- modul-1/230441100087-AndikaKamaluddin/slicing/src/slicing/Slicing.java
+++ modul-1/230441100087-AndikaKamaluddin/slicing/src/slicing/Slicing.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package pertemuan1;
+package slicing;
 
 /**
  *
  * @author USEr
  */
-public class Pertemuan1 {
+public class Slicing {
 
     /**
      * @param args the command line arguments
