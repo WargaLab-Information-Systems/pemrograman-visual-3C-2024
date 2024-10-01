1728c9fa9a687c5c8b19faca6a6e6aa949312476
uiqwdiuuqi <emailgitzainularifin4216@gmail.com>
2024-10-01 21:51:19 +0700

    ZainulArifin

--- modul-1/230441100159ZainulArifin/TP1.java
+++ modul-1/230441100159ZainulArifin/TP1.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package praktikums3;
+package tp1;
 
 /**
  *
  * @author User
  */
-public class PraktikumS3 {
+public class TP1 {
 
     /**
      * @param args the command line arguments
