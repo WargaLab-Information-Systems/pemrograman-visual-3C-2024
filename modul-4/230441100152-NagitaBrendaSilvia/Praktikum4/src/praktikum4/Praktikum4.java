e59ed272f7435b58877e0a085d64ef85625217d4
NagitaBS <143859860+NagitaBS@users.noreply.github.com>
2024-10-29 21:21:52 +0700

    230441100152-NAGITABRENDASILVIA-KAKSAMSULHADI-MODUL4 (#169)

---
+++ modul-4/230441100152-NagitaBrendaSilvia/Praktikum4/src/praktikum4/Praktikum4.java
@@ -0,0 +1,12 @@
+package praktikum4;
+
+
+public class Praktikum4 {
+
+    
+    public static void main(String[] args) {
+       FRM_GameTebak n = new FRM_GameTebak();
+        n.setVisible(true);
+    }
+    
+}
