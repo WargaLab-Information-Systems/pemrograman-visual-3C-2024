9ac012a358c5217366e700b0917048d08dc9b42f
Isa'atul Maghfiroh <143859503+IsaatulMaghfiroh@users.noreply.github.com>
2024-11-06 17:18:44 +0700

    230441100068_Isa'atulMaghfiroh_Modul3_KakMartin (#233)
    * latihan
    
    * 230441100068_Isa'atulMaghfiroh_ImanuelMartinJ.Harahap-Modul1
    
    * 230441100068_Isa'atulMaghfiroh_Modul2_KakMartin
    
    * martin
    
    * 230441100068_Isa'atulMaghfiroh_Modul3_KakMartin

---
+++ modul-3/230441100068_Isa'atulMaghfiroh/PEMVIS_MODUL3_FIFI/src/pemvis_modul3/PEMVIS_MODUL3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pemvis_modul3;
+
+/**
+ *
+ * @author HP
+ */
+public class PEMVIS_MODUL3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
