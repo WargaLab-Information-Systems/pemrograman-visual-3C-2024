b01220908591b0ca8aa4bdc9383c110dc681db09
Ssuryabaihaqi480 <163401698+Ssuryabaihaqi480@users.noreply.github.com>
2024-11-06 16:54:13 +0700

    23 - 082_SURYA BAIHAQI AHMAD_MODUL_4 (#195)
    * latihan pertama
    
    * 230441100082_SURYA BAIHAQI AHMAD_MODUL 1_KAK MARTIN
    
    * 230441100082_SURYA BAIHAQI AHMAD_MODUL 2_KAK MARTIN
    
    * 230441100082_SURYA BAIHAQI AHMAD_MODUL3_KAK MARTIN
    
    * 230441100082_SURYA BAIHAQI AHMAD_MODUL 4_KAK MARTIN

---
+++ modul-3/230441100082_SURYA BAIHAQI AHMAD_MODUL 3/Modul3_Apotek1/src/modul3_apotek1/Modul3_Apotek1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul3_apotek1;
+
+/**
+ *
+ * @author H
+ */
+public class Modul3_Apotek1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
